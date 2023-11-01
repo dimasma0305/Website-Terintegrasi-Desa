d51502f9dffa102ed901292098497c84c3c0d2bf
dimasma0305 <dimasmaulana0305@gmail.com>
2023-11-01 13:24:19 +0800

    update html

--- html/application/views/index.php
+++ html/application/views/index.php
@@ -10,6 +10,6 @@
 <?php $this->load->view('includes/navbar')?>
 <main class="container">
 </main>
-<script src='/static/js/bootstrap.bundle.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'></script>
 </body>
 </html>
