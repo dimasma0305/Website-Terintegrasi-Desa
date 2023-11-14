840774b21ba9b7cd90eec7d431dbad942d79cd4a
dimasma0305 <dimasmaulana0305@gmail.com>
2023-11-14 12:15:30 +0800

    add login and register controller

--- html/application/controllers/Cindex.php
+++ html/application/controllers/Cindex.php
@@ -3,10 +3,54 @@
 
 class Cindex  extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("muser");
+	}
+
 	function index() {
 		$this->load->view('includes/header');
 		$this->load->view('partials/navbar');
 	    $this->load->view("index");
 		$this->load->view('includes/footer');
+	}
+	function  login() {
+		if ($this->input->method() === "get"){
+			$this->load->view('includes/header');
+			$this->load->view('partials/navbar');
+			$this->load->view('login');
+			$this->load->view('includes/footer');
+		} elseif ($this->input->method() === 'post'){
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			try {
+				$user = $this->muser->verifyUser($username, $password);
+				$this->session->set_userdata($user);
+				redirect(base_url('/'), 'refresh');
+			} catch (Throwable $err){
+				$this->session->set_flashdata('error', $err->getMessage());
+				redirect(base_url('cindex/login'), 'refresh');
+			}
+		}
+	}
+	function register() {
+		if ($this->input->method() === "get"){
+			$this->load->view('includes/header');
+			$this->load->view('partials/navbar');
+			$this->load->view('register');
+			$this->load->view('includes/footer');
+		} elseif ($this->input->method() === 'post'){
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			try {
+				$user = $this->muser->createUser($username, $password);
+				$this->session->set_userdata($user);
+				redirect(base_url('/'), 'refresh');
+			} catch (Throwable $err){
+				$this->session->set_flashdata('error', $err->getMessage());
+				redirect(base_url('cindex/register'), 'refresh');
+			}
+		}
 	}
 }
