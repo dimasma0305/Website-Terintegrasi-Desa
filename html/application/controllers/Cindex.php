9d933b464d4c9a216eba263b2ce9ecdad98371c0
gunturdwiap <kadekguntur13@gmail.com>
2023-12-01 13:52:23 +0800

    Add dashboard template

--- html/application/controllers/Cindex.php
+++ html/application/controllers/Cindex.php
@@ -10,10 +10,11 @@ class Cindex  extends CI_Controller
 	}
 
 	function index() {
-		$this->load->view('includes/header');
-		$this->load->view('partials/navbar');
-	    $this->load->view("index");
-		$this->load->view('includes/footer');
+		// $this->load->view('includes/header');
+		// $this->load->view('partials/navbar');
+	    // $this->load->view("index");
+		// $this->load->view('includes/footer');
+		$this->load->view('dashboard_template');
 	}
 
 	function login_template() {
