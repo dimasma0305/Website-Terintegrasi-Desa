2e172285a52a1fcd670c00e71404f03e034bdab2
gunturdwiap <kadekguntur13@gmail.com>
2023-12-06 20:02:19 +0800

    Update navbar template

--- html/application/views/partials_template/sidebar_template.php
+++ html/application/views/partials_template/sidebar_template.php
@@ -1,6 +1,19 @@
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<?php
 
+    function activeLink($menu)
+    {
+        $ci = get_instance();
+        if ($ci->uri->uri_string() == $menu) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+?>
+
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
@@ -8,13 +21,18 @@
                 </div>
                 <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
             </a>
-
+            
             <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+            <hr class="sidebar-divider">
+            
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Dashboard
+            </div>
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
+            <li class="nav-item <?= activeLink('dashboard') ?>">
+                <a class="nav-link" href="<?= base_url('dashboard') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -28,16 +46,16 @@
             </div>
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+            <li class="nav-item <?= activeLink('form_surat') ?>">
+                <a class="nav-link" href="<?= base_url('form_surat') ?>">
+                    <i class="fas fas-fw fa-envelope-open-text"></i>
                     <span>Form Surat</span></a>
             </li>
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+            <li class="nav-item <?= activeLink('daftar_surat') ?>">
+                <a class="nav-link" href="<?= base_url('daftar_surat') ?>">
+                    <i class="fas fa-fw fa-mail-bulk"></i>
                     <span>Daftar Surat</span></a>
             </li>
 
@@ -50,16 +68,16 @@
             </div>
 
             <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
+            <li class="nav-item <?= activeLink('profil') ?>">
+                <a class="nav-link" href="<?= base_url('profil') ?>">
+                    <i class="fas fa-fw fa-user"></i>
                     <span>Profil</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
+            <li class="nav-item <?= activeLink('edit_profil') ?>">
+                <a class="nav-link" href="<?= base_url('edit_profil') ?>">
+                    <i class="fas fa-fw fa-user-edit"></i>
                     <span>Edit Profil</span></a>
             </li>
 
