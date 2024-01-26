497fce6c93b80da5881fa4146767e5bc2e0e2a5f
dimasma0305 <dimasmaulana0305@gmail.com>
2024-01-26 18:11:19 +0800

    feat: remove htmls

--- htmls/application/views/includes/footer.php
+++
@@ -1,4 +0,0 @@
-
-<script src='<?=base_url('/static/js/bootstrap.bundle.min.js')?>'></script>
-</body>
-</html>
--- htmls/application/views/index.php
+++
@@ -1,8 +0,0 @@
-<main class="container">
-<div class="row">
-	<div class="col-12 text-center">
-		<h1>Hello World <?=$this->session->username?></h1>
-	</div>
-</div>
-</main>
-
